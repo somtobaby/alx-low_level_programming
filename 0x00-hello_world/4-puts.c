d8094fe98a7bb12f4dfb939b8b130c2abe2680ba
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-09 16:40:21 -0700

    S

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts (Programming is like building a multilingual puzzle)
+#include <stdio.h> 
+
+int main(void)
+{
+            //string initialisation
+        char Mystr[] = "Programming is like building a multilingual puzzle";
+	
+	puts(Mystr); //writing the string to stdout
+       
+	return 0;
+	
+}	
