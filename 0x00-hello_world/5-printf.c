5b6eb8478407dda57522f3cd62084c0f48e7e158
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-10 14:30:44 -0700

    I am so happy

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry of the program
+ * description - i think i am getting the hang of this
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
