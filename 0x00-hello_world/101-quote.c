3c7eae0296ed2bc220e6f1241ac4b9b4a750477c
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-10 16:42:53 -0700

    S

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * description - printing a quote to standard error
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar 20-10-19\n");
+	return (1);
+}
