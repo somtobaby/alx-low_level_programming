48334a6a886d3fe851d26ff2d669fdb74166d096
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-11 19:05:54 -0700

    SR did it again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int longlongintType;
 	float floatType;
 
-	printf("Size of char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of int: %ld byte(s)\n", sizeof(intType));
-	printf("Size of long int: %ld byte(s)\n", sizeof(longintType));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(longlongintType));
-	printf("Size of float: %ld byte(s)\n\n", sizeof(floatType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
