cf8bb53d8d4bd377832fbb1d8e9020f9c4935461
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-12 14:29:57 -0700

    SR

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the program
+ * decription - code to print th numbers in base ten
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str = "0123456789";
+
+	puts(str);
+
+	return (0);
+
+}
