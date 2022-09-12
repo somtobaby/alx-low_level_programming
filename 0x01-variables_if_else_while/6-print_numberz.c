bd223ea4a24099fbfcf6eda0ac591aeab3e7f22c
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-12 14:38:28 -0700

    SR

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the program
+ * description - code to find the digits in base ten
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str = "0123456789";
+
+	puts(str);
+
+	return (0);
+}
