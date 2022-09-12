9d8a0d949190e22819a194a845729c42ed79faca
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-12 13:06:52 -0700

    SR

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the program
+ * description - code that prints alphabet in upper and lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str = "abcdefghijklmnopqrstuvwxyz"
+
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		puts(str);
+
+	return (0);
+}
