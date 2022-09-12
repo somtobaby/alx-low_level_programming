d2fd6b771f8aa936671c9045d98034946091a710
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-12 13:17:45 -0700

    SR

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * description - code that prints the alphabet except q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str = "abcdfghijklmnoprstuvwxyz";
+	puts(str);
+	
+	return (0);
+}
