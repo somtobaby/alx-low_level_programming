eedab30c47867def15b548ceb8a9ef1a44cc262d
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-12 11:10:46 -0700

    SR

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the starting point of the program
+ * description - code to diaplay lowercase alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+
+	{
+		printf("%c", c);
+
+	}
+	return (0);
+}
