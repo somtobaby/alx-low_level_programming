c0ec430afb500631fffaa0609b51a9c700066348
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-12 12:40:42 -0700

    SR

---
+++ 0x00-hello_world/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * description -  code to print the alphabet in upper and lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str = "abcdefghijklmnopqrstuvwxyz"
+
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		puts(str);
+	
+		return (0);
+}
