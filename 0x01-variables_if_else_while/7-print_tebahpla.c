94cfc220738bbf606c217fa3fd88a990b6da1607
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-12 18:02:24 -0700

    SR

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * description - Displays the alphabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+	
+	for (alphabet = 'z'; alphabet >= 'z'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+	return (0);
+}
