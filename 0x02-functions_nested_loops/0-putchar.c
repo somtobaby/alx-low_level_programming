13bec8d7913f60218416a253d25d63c984fcb5f2
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-14 06:59:12 -0700

    SR

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * description -  program that prints the string putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str = "_putchar";
+
+	puts(str);
+
+	return (0);
+}
