5a51053fbbe87b967a1ea8eccfe88a3206fa136e
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-15 11:42:18 -0700

    SR

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "mainn.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -12,7 +12,7 @@ int main(void)
 	int i;
 
 	i = 0;
-	positive_or_negative(i)
+	positive_or_negative(i);
 
 	return (0);
 }
