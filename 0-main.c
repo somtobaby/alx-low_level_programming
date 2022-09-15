6b174286597de930f182696030dd42a00b173455
Okoyenta Somtochukwu <okoyentarita@gmail.com>
2022-09-15 11:29:39 -0700

    SR

---
+++ 0-main.c
@@ -0,0 +1,18 @@
+#include "mainn.h"
+#include <stdio.h>
+
+/**
+ * main - tests function that prints if integer is positive ornegative
+ * description - donno
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i)
+
+	return (0);
+}
